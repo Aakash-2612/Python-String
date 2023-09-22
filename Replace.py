00fd77d614c3472f8a148b3ef18bc74b0b7755ca
Aakash Kumar <aakashkumaryadav36@gmail.com>
2023-09-22 11:49:32 +0530

    second commit

--- Replace.py
+++ Replace.py
@@ -7,7 +7,7 @@ for i in range(len(str1)):
 
 print(str2)
 
-list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 str3 = ""   
 for i in list1:
     if(i%2 == 0):
